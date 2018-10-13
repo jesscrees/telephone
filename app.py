f80fb3beabfbd5ff6be3d3a3ba75413ce9592fdb
Sharla Kew <sharla.kew@futurelearn.com>
2018-10-13 13:03:46 +0100

    add rough estimate of nexmo api calls we may need

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import nexmo
 NEXMO_API_KEY = os.environ['NEXMO_API_KEY']
 NEXMO_SECRET_KEY = os.environ['NEXMO_SECRET_KEY']
 
+client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
+
+# make a call
+response = client.create_call({
+    'to': [{'type': 'phone', 'number': '14843331234'}],
+    'from': {'type': 'phone', 'number': '14843335555'},
+    'answer_url': ['https://example.com/answer']
+})
+
+# stream sybnthesized speech to a call
+response = client.send_speech(uuid, text='Still working right now at least.')
+
+#Stop sending a synthesized speech message to a call
+response = client.stop_speech(uuid)
