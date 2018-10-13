9f2d0cee55c3884b54d9b6793172b33d2820fee1
Sharla Kew <sharla.kew@futurelearn.com>
2018-10-13 17:33:55 +0100

    basic phone call is working

--- app/app.py
+++ app/app.py
@@ -2,20 +2,27 @@ import os
 import nexmo
 
 
-NEXMO_API_KEY = os.environ['NEXMO_API_KEY']
-NEXMO_SECRET_KEY = os.environ['NEXMO_SECRET_KEY']
+# NEXMO_API_KEY = os.environ['NEXMO_API_KEY']
+# NEXMO_SECRET_KEY = os.environ['NEXMO_SECRET_KEY']
+NEXMO_APPLICATION_ID = os.environ['NEXMO_APPLICATION_ID']
+NEXMO_PRIVATE_KEY = os.environ['NEXMO_PRIVATE_KEY']
+TEST_PHONE = os.environ['TEST_PHONE']
 
-client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
+client = nexmo.Client(application_id=NEXMO_APPLICATION_ID, private_key=NEXMO_PRIVATE_KEY)
 
+number_to_call = TEST_PHONE
+ 
 # make a call
 response = client.create_call({
-    'to': [{'type': 'phone', 'number': '14843331234'}],
-    'from': {'type': 'phone', 'number': '14843335555'},
-    'answer_url': ['https://example.com/answer']
+    'to': [{'type': 'phone', 'number': number_to_call}],
+    'from': {'type': 'phone', 'number': number_to_call},
+    'answer_url': ['http://cb9df9b5.ngrok.io']
 })
 
+print(response)
+
 # stream sybnthesized speech to a call
-response = client.send_speech(uuid, text='Still working right now at least.')
+# response = client.send_speech(uuid, text='Still working right now at least.')
 
 #Stop sending a synthesized speech message to a call
-response = client.stop_speech(uuid)
+# response = client.stop_speech(uuid)
